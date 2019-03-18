84d8cbfa80da126f1cd07d77d491fb147c3d157e
罗凯 <17301858781@163.com>
2019-03-18 16:59:34 +0800

    几道LeetCode

---
+++ source/_posts/leetcode/Nine.java
@@ -0,0 +1,64 @@
+/**
+ * 移动零
+ * <p>
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ * <p>
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ */
+public class Nine {
+
+    public static void main(String[] args) {
+        int[] nums1 = {0, 1, 0, 3, 12};
+        moveZeroes1(nums1);
+        for (int i : nums1) {
+            System.out.print(i);
+        }
+    }
+
+    /**
+     * 解题思路：
+     *  直接遍历数组，将不为0的元素依次排序，剩余的元素标记为0即可。
+     *  这种方法的执行次数较少，不关注移动元素的过程，只关注最终结果
+     */
+    public static void moveZeroes(int[] nums) {
+        if(nums == null || nums.length == 0) return;
+
+        int index = 0;
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    /**
+     * 解题思路：
+     *  从后向前遍历，记录出现0前的位数，将后几位依次前移
+     */
+    public static void moveZeroes1(int[] nums){
+        if(nums == null || nums.length == 0) return;
+        int count = 0;
+        int size  = 1;
+        for (int i = nums.length - 1; i >= 0 ; i--) {
+            if(nums[i] == 0 && count > 0){
+                for (int j = nums.length - count; j < nums.length; j++) {
+                    nums[j - 1] = nums[j];
+                }
+                nums[nums.length - size++] = 0;
+                count ++;
+            }else{
+                count ++ ;
+            }
+        }
+    }
+
+}
--- source/_posts/leetcode/Seven.java
+++ source/_posts/leetcode/Seven.java
@@ -34,6 +34,9 @@ public class Seven {
         System.out.println("方法共执行了："+(System.currentTimeMillis() - start));
     }
 
+    /**
+     * 使用集合的特性
+     */
     public static int[] intersect(int[] nums1, int[] nums2) {
         if(nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0) return new int[0];
 
@@ -58,6 +61,32 @@ public class Seven {
             result[point++] = integer;
         }
 
+        return result;
+    }
+
+    /**
+     * 效率高于第一种方法
+     */
+    public static int[] intersect2(int[] nums1, int[] nums2) {
+        // 先将数组排序，数组将按照从小到大的顺序排列
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        // 定义下标指针
+        int i = 0, j = 0, k = 0;
+        // 创建一个数组，长度为小数组的长度
+        int[] result = new int[nums1.length > nums2.length ? nums2.length : nums1.length];
+        // 两个数组同时遍历，比较 i与j所在的值，只会有三种情况：大、小、等
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) { // 等于则保存起来，i,j 向前+1
+                result[k++] = nums1[i];
+                i++;
+                j++;
+            }else if(nums1[i] < nums2[j]){ // i<j i向前+1
+                i++;
+            }else if(nums1[i] > nums2[j]){
+                j++;
+            }
+        }
         return result;
     }
 }
