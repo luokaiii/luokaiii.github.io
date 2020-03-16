b8ef1bdf391857a05d412ea6c0b57ec477e59815
luokaiii <133@163.com>
2020-03-16 11:15:39 +0800

    Site updated: 2020-03-16 11:15:25

--- js/scroll.js
+++ js/scroll.js
@@ -53,7 +53,7 @@ $(function () {
   })
 
   // head scroll
-  $('h1,h2,h3,h4,h5,h6').on('click', function (e) {
+  $('#post-content').find('h1,h2,h3,h4,h5,h6').on('click', function (e) {
     scrollToHead('#' + $(this).attr('id'))
   })
 
@@ -80,6 +80,9 @@ $(function () {
 
   // expand toc-item
   function expandToc ($item) {
+    if ($item.is(':visible')) {
+      return
+    }
     $item.velocity('stop').velocity('transition.fadeIn', {
       duration: 500,
       easing: 'easeInQuart'
@@ -92,7 +95,9 @@ $(function () {
     var contentMath = (docHeight > winHeight) ? (docHeight - winHeight) : ($(document).height() - winHeight)
     var scrollPercent = (currentTop) / (contentMath)
     var scrollPercentRounded = Math.round(scrollPercent * 100)
-    var percentage = (scrollPercentRounded > 100) ? 100 : scrollPercentRounded
+    var percentage = (scrollPercentRounded > 100) ? 100
+      : (scrollPercentRounded <= 0) ? 0
+        : scrollPercentRounded
     $('.progress-num').text(percentage)
     $('.sidebar-toc__progress-bar').velocity('stop')
       .velocity({
@@ -103,13 +108,24 @@ $(function () {
       })
   }
 
-  // find head position & add active class
-  function findHeadPosition (top) {
-    if (top < 200) {
-      $('.toc-link').removeClass('active')
-      $('.toc-child').hide()
+  function updateAnchor (anchor) {
+    if (window.history.replaceState && anchor !== window.location.hash) {
+      window.history.replaceState(undefined, undefined, anchor)
     }
-    var list = $('h1,h2,h3,h4,h5,h6')
+  }
+
+  // find head position & add active class
+  // DOM Hierarchy:
+  // ol.toc > (li.toc-item, ...)
+  // li.toc-item > (a.toc-link, ol.toc-child > (li.toc-item, ...))
+  function findHeadPosition (top) {
+    // assume that we are not in the post page if no TOC link be found,
+    // thus no need to update the status
+    if ($('.toc-link').length === 0) {
+      return false
+    }
+
+    var list = $('#post-content').find('h1,h2,h3,h4,h5,h6')
     var currentId = ''
     list.each(function () {
       var head = $(this)
@@ -117,25 +133,32 @@ $(function () {
         currentId = '#' + $(this).attr('id')
       }
     })
+
+    if (currentId === '') {
+      $('.toc-link').removeClass('active')
+      $('.toc-child').hide()
+    }
+
     var currentActive = $('.toc-link.active')
     if (currentId && currentActive.attr('href') !== currentId) {
+      updateAnchor(currentId)
+
       $('.toc-link').removeClass('active')
       var _this = $('.toc-link[href="' + currentId + '"]')
       _this.addClass('active')
+
       var parents = _this.parents('.toc-child')
-      if (parents.length > 0) {
-        var child
-        parents.length > 1 ? child = parents.eq(parents.length - 1).find('.toc-child') : child = parents
-        if (child.length > 0 && child.is(':hidden')) {
-          expandToc(child)
-        }
-        parents.eq(parents.length - 1).closest('.toc-item').siblings('.toc-item').find('.toc-child').hide()
-      } else {
-        if (_this.closest('.toc-item').find('.toc-child').is(':hidden')) {
-          expandToc(_this.closest('.toc-item').find('.toc-child'))
-        }
-        _this.closest('.toc-item').siblings('.toc-item').find('.toc-child').hide()
-      }
+      // Returned list is in reverse order of the DOM elements
+      // Thus `parents.last()` is the outermost .toc-child container
+      // i.e. list of subsections
+      var topLink = (parents.length > 0) ? parents.last() : _this
+      expandToc(topLink.closest('.toc-item').find('.toc-child'))
+      topLink
+        // Find all top-level .toc-item containers, i.e. sections
+        // excluding the currently active one
+        .closest('.toc-item').siblings('.toc-item')
+        // Hide their respective list of subsections
+        .find('.toc-child').hide()
     }
   }
 })
